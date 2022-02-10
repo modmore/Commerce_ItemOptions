1929c7a35da74dad3bf8d832666c73996fb611bb
Mark Hamstra <hello@markhamstra.com>
2022-02-10 17:02:19 +0100

    Handle submitted options on add to cart

--- core/components/commerce_itemoptions/lexicon/en/default.inc.php
+++ core/components/commerce_itemoptions/lexicon/en/default.inc.php
@@ -2,3 +2,5 @@
 
 $_lang['commerce_itemoptions'] = 'Item Options';
 $_lang['commerce_itemoptions.description'] = 'Provides a flexible solution to configuring order items, by allowing separate products to be provided as additional options.';
+$_lang['commerce_itemoptions.allowed_fields'] = 'Allowed Fields';
+$_lang['commerce_itemoptions.allowed_fields.desc'] = 'A comma separated list of field keys that will be accepted as options to a product. For example: accessory1, accessory2, insurance';
--- core/components/commerce_itemoptions/src/Modules/ItemOptions.php
+++ core/components/commerce_itemoptions/src/Modules/ItemOptions.php
@@ -1,10 +1,15 @@
 <?php
 namespace modmore\Commerce_ItemOptions\Modules;
 
+use comOrderItemExtraAdjustment;
+use comProduct;
 use modmore\Commerce\Admin\Configuration\About\ComposerPackages;
 use modmore\Commerce\Admin\Sections\SimpleSection;
+use modmore\Commerce\Admin\Widgets\Form\TextField;
 use modmore\Commerce\Events\Admin\PageEvent;
+use modmore\Commerce\Events\Cart\Item;
 use modmore\Commerce\Modules\BaseModule;
+use modX;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
@@ -41,19 +46,63 @@ class ItemOptions extends BaseModule {
 //        $root = dirname(__DIR__, 2);
 //        $this->commerce->view()->addTemplatesPath($root . '/templates/');
 
+        $dispatcher->addListener(\Commerce::EVENT_ITEM_ADDED_TO_CART, [$this, 'onAddToCart']);
+
         // Add composer libraries to the about section (v0.12+)
         $dispatcher->addListener(\Commerce::EVENT_DASHBOARD_LOAD_ABOUT, [$this, 'addLibrariesToAbout']);
     }
 
-    public function getModuleConfiguration(\comModule $module)
+    public function onAddToCart(Item $event): void
+    {
+        $item = $event->getItem();
+        $keys = $this->getAllowedKeys();
+
+        foreach ($keys as $key) {
+            $val = (int)$event->getOption($key);
+            if (empty($val)) {
+                continue;
+            }
+
+            /** @var comProduct $product */
+            $product = $this->adapter->getObject(comProduct::class, ['id' => $val]);
+            if (!$product) {
+                $this->adapter->log(modX::LOG_LEVEL_WARN, "[ItemOptions] Received {$key}={$val}, however no product with ID {$val} found.");
+                continue;
+            }
+
+            $name = $product->getName();
+            $price = $product->getPricing($this->commerce->currency)->getPriceForItem($item)->getInteger();
+            $quantity = max(1, (int)$event->getOption($key . '_quantity', 1));
+            if ($quantity > 1) {
+                $name = "{$quantity}x {$name}";
+                $price *= $quantity;
+            }
+
+            /** @var comOrderItemExtraAdjustment $adjustment */
+            $adjustment = $this->adapter->newObject(comOrderItemExtraAdjustment::class);
+            $adjustment->fromArray([
+                'key' => 'itemopt_' . $key,
+                'name' => $name,
+                'price_change' => $price,
+                'price_change_per_quantity' => true,
+                'show_on_order' => true,
+            ]);
+
+            $item->addPriceAdjustment($adjustment);
+        }
+
+    }
+
+    public function getModuleConfiguration(\comModule $module): array
     {
         $fields = [];
 
-        // A more detailed description to be shown in the module configuration. Note that the module description
-        // ({@see self:getDescription}) is automatically shown as well.
-//        $fields[] = new DescriptionField($this->commerce, [
-//            'description' => $this->adapter->lexicon('commerce_itemoptions.module_description'),
-//        ]);
+        $fields[] = new TextField($this->commerce, [
+            'label' => $this->adapter->lexicon('commerce_itemoptions.allowed_fields'),
+            'description' => $this->adapter->lexicon('commerce_itemoptions.allowed_fields.desc'),
+            'name' => 'properties[allowed_fields]',
+            'value' => $module->getProperty('allowed_fields', ''),
+        ]);
 
         return $fields;
     }
@@ -72,5 +121,12 @@ class ItemOptions extends BaseModule {
             $about = $event->getPage();
             $about->addSection($section);
         }
+    }
+
+    private function getAllowedKeys()
+    {
+        $fields = $this->getConfig('allowed_fields', '');
+        $fields = array_map('trim', explode(',', $fields));
+        return array_filter($fields);
     }
 }
