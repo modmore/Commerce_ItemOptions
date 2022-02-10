f810cbf2e5127c09da495eaf3149741f62b7e9d3
Mark Hamstra <hello@markhamstra.com>
2022-02-10 14:21:59 +0100

    Standard module bootstrap

---
+++ core/components/commerce_itemoptions/lexicon/en/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_itemoptions'] = 'Item Options';
+$_lang['commerce_itemoptions.description'] = 'Provides a flexible solution to configuring order items, by allowing separate products to be provided as additional options.';
